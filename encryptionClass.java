3d6c63f5dcce8f0fd29edd2ad205bfccdfdd7710
Maximus <jain.aiyush@gmail.com>
2017-02-23 21:03:34 -0500

    Create encryptionClass.java

---
+++ encryptionClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author aiyush
+ *
+ */
+public class encryptionClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
